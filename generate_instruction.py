2ac27de8365172dd8189e95f7c00fdf4140dcb58
Phodal Huang <h@phodal.com>
2023-03-31 22:20:32 +0800

    docs: update for regen

--- generate_instruction.py
+++ generate_instruction.py
@@ -4,7 +4,7 @@ batch_selfinstruct_generate.py
 run:
 python -m generate_instruction generate_instruction_following_data \
   --output_dir ./ \
-  --num_instructions_to_generate 10 \
+  --num_instructions_to_generate 20 \
   --model_name="text-davinci-003" \
 """
 import time
